a583ecee92aac1acdd230d7160cf3e64a89edbde
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2024-08-05 16:19:28 -0300

    Update main.py

--- Tarde/Aula 30/TesteBD/main.py
+++ Tarde/Aula 30/TesteBD/main.py
@@ -1,38 +1,37 @@
 import mysql.connector
-import credencial
 
-host = credencial.host
-user = credencial.user
-password = credencial.password
-database = credencial.database
+host = "localhost"
+user = "root"
+password = "mysql"
+database = "escol"
 
-conexaoBD = mysql.connector.connect(host=host, user=user, password=password, database=database)
+conexaoBD = None
+cursor = None
 
-print("Conexão Estabelecida!")
+try:
+    conexaoBD = mysql.connector.connect(host=host, user=user, password = password, database=database)
 
-cursor = conexaoBD.cursor()
+    print("Conexão Estabelecida!")
+    cursor = conexaoBD.cursor()
+    
+    #OPERAÇÕES SQL 
+    
+    #Visualizar os alunos do banco escola no padrão
+    #<matricula>. <nome> - <cpf>
+    
+    #Peça ao usuário um número de matricula e exiba as informações completas do aluno escolhido
+    #Dica: cursor.execute(f"SELECT * FROM aluno WHERE matriculaaluno = {matricula}")
+       
+except mysql.connector.Error as e:
+    print(e.msg)
+finally:
+    if cursor != None:
+        cursor.close()
+    if conexaoBD != None:
+        conexaoBD.close()
+        
+        
+# 
+    
 
-cursor.execute("SELECT VERSION()")
 
-resultado = cursor.fetchall()
-
-print(resultado)
-#Imprimir na tela a mensagem "A VERSÃO DO MYSQL É x.xxx"
-print(f"A VERSÃO DO MYSQL É {resultado[0][0]}")
-
-#Faça uma consulta e exiba na tela todos os alunos cadastrados na tabela aluno
-
-cursor.execute("SELECT * FROM aluno")
-resultado = cursor.fetchall()
-
-print(resultado)
-
-#Exiba na tela a matricula e nome do primeiro aluno e do último aluno. No formato "<matricula>. <nome>"
-print(f"{resultado[0][0]}. {resultado[0][2]} - {resultado[0][6]}")
-
-print(f"{resultado[-1][0]}. {resultado[-1][2]} - {resultado[-1][-1]}")
-
-#Imprima todos os alunos no formato "<matricula>. <nome>"
-
-for aluno in resultado:
-    print(f"{aluno[0]}. {aluno[2]}")
