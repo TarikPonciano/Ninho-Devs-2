6f7cdfd25687a08643249003364caaf3d4dc53d1
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2024-08-23 14:02:29 -0300

    Update Main.py

--- Tarde/Aula 35/Mercadinho/Main.py
+++ Tarde/Aula 35/Mercadinho/Main.py
@@ -11,11 +11,20 @@ def verProdutos():
         espacoNome = ' ' * round(15-(len(produto[1])/2))
         print(f"{produto[0]} |{espacoNome}{produto[1]}{espacoNome}| {produto[2]} | {produto[3]}")
         
-        
-        
-        
-        
+def buscarProdutoPorId(id):
+    #Função que busca o banco por um produto específico usando o id e retorna a tupla do primeiro produto do resultado se não retorna None
+    
+    resultado = conexaoBD.consultarComParametros("SELECT * FROM produtos WHERE id_produto = %s", (id,)) 
+    [()]
+    
+    if (resultado == []):
+        return None
+    else:
+        return resultado[0]
+    
+    
 
+            
 # Crie um programa que exibe um menu com as opções:
 # 1. Ver Produtos
 # 2. Cadastrar Venda
@@ -41,12 +50,25 @@ while True:
         verProdutos()
     elif (op == "2"):
         # Mostrar a tabela de produtos para o usuário
+        verProdutos()
         
-        # Pedir o id dos produtos da compra
-        #   - Verifica se o produto existe
-        
+        while True:
+            # Pedir o id dos produtos da compra
+            idProduto = int(input("Digite o id do produto:"))
+            
+            #   - Verifica se o produto existe no banco de dados
+            # Consulta o banco pra ver se o produto com id específico existe
+            produto = buscarProdutoPorId(idProduto) #Retornar uma tupla
+            
+            if produto == None:
+                print("Produto não encontrado! Digite um id válido!")
+            else:
+                print(f"Você escolheu {produto[1]}")
+                break
+
         # Pedir a quantidade de unidades compradas daquele produto
         #   - Verificar se o estoque é suficiente
+        
         # Criar a venda (Inserir Venda)
         # Criar os registros de venda dos produtos (Inserir na tabela itens)
         # Atualizar a quantidade de produto na tabela produto
