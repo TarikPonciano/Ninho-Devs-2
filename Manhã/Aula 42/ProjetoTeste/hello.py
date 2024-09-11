d017a4ce8caa45d8276295df71c042aca0390337
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2024-09-11 08:23:56 -0300

    Aula 42

---
+++ Manhã/Aula 42/ProjetoTeste/hello.py
@@ -0,0 +1,5 @@
+# Criar um repositório chamado "Meu primeiro Repo" (git init)
+# Criar dentro do repositório um arquivo chamado hello.txt
+# Adicionar esse arquivo ao staging (git add <nome>)
+# Commitar essa mudança no repositório (git commit -m "")
+# Ver o histórico de mudanças (git log)
