94d1c23268cfa3aea1adc25404d5f493371fe9a2
Tarik Ponciano <tarikponciano@gmail.com>
2024-09-13 11:05:21 -0300

    Atualização Imagem

--- Manhã/Aula 44/LayoutsPyQt/layouts com imagem.py
+++ Manhã/Aula 44/LayoutsPyQt/layouts com imagem.py
@@ -1,6 +1,8 @@
 import sys
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QGridLayout, QFormLayout, QSizePolicy
+from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import QSize
 
 class JanelaPrincipal (QMainWindow):
     
@@ -16,21 +18,20 @@ class JanelaPrincipal (QMainWindow):
         self.caixaMenuPrincipal.setStyleSheet("QWidget{ background-color: red;} QPushButton{background-color: #f0f0f0;}")
         
         self.layoutMenuPrincipal = QVBoxLayout()
-        self.layoutMenuPrincipal.setSpacing(10)
-        self.layoutMenuPrincipal.setContentsMargins(50,10,50,10)
+        
         
         self.botao1 = QPushButton("Botao 1")
-        self.botao1.setSizePolicy(QSizePolicy.Policy.Expanding,  QSizePolicy.Policy.Expanding)
+        self.botao1.setIcon(QIcon("images/instagram-logo.png"))
+        self.botao1.setIconSize(QSize(32,32))
+        self.botao1.setStyleSheet("background-color:white; border-radius: 16px; color:gray;")
         
+        self.botao1.setSizePolicy(QSizePolicy.Policy.Expanding,QSizePolicy.Policy.Expanding)
         self.botao2 = QPushButton("Botao 2")
-        self.botao2.setSizePolicy(QSizePolicy.Policy.Expanding,  QSizePolicy.Policy.Expanding)
-        
+        self.botao2.setSizePolicy(QSizePolicy.Policy.Expanding,QSizePolicy.Policy.Expanding)
         self.botao3 = QPushButton("Botao 3")
-        self.botao3.setSizePolicy(QSizePolicy.Policy.Expanding,  QSizePolicy.Policy.Expanding)
-        
+        self.botao3.setSizePolicy(QSizePolicy.Policy.Expanding,QSizePolicy.Policy.Expanding)
         self.botao4 = QPushButton("Botao 4")
-        self.botao4.setSizePolicy(QSizePolicy.Policy.Expanding,  QSizePolicy.Policy.Expanding)
-        
+        self.botao4.setSizePolicy(QSizePolicy.Policy.Expanding,QSizePolicy.Policy.Expanding)
         
         self.layoutMenuPrincipal.addWidget(self.botao1)
         self.layoutMenuPrincipal.addWidget(self.botao2)
