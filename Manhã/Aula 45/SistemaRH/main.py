c382a0d092a6f2e038be89ca3749ca23a9684d62
Tarik Ponciano <tarikponciano@gmail.com>
2024-09-16 09:31:52 -0300

    Update main.py

--- Manhã/Aula 45/SistemaRH/main.py
+++ Manhã/Aula 45/SistemaRH/main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QStackedLayout, QPushButton, QMainWindow
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QStackedLayout, QPushButton, QMainWindow, QSizePolicy
 from PyQt6.QtGui import QFont
 from PyQt6.QtCore import Qt
 
@@ -28,10 +28,29 @@ class JanelaPrincipal(QMainWindow):
         
     def criarMenuLateral(self):
         menuLateral = QWidget()
-        menuLateral.setStyleSheet("QWidget {background-color:rgb(180,0,0); border:3px solid black;}")
+        menuLateral.setStyleSheet('''QWidget {background-color:rgb(180,0,0); border:3px solid black;} 
+        QPushButton {background-color: rgb(0,0,150); color: white; border: 2px outset black; border-radius:30px; height: 100%;}
+        ''')
         layoutMenuLateral = QVBoxLayout()
-        layoutMenuLateral.setSpacing(15)
+        layoutMenuLateral.setSpacing(25)
         layoutMenuLateral.setContentsMargins(50, 100, 0, 200)
+        
+        botaoVerFuncionarios = QPushButton("Ver Funcionarios")
+        
+        layoutMenuLateral.addWidget(botaoVerFuncionarios)
+        
+        botaoInserirFuncionario = QPushButton("Inserir Funcionario")
+        
+        layoutMenuLateral.addWidget(botaoInserirFuncionario)
+        
+        botaoAlterarFuncionario = QPushButton("Alterar Funcionario")
+        layoutMenuLateral.addWidget(botaoAlterarFuncionario)
+        
+        botaoRemoverFuncionario = QPushButton("Remover Funcionario")
+        layoutMenuLateral.addWidget(botaoRemoverFuncionario)
+        
+        botaoSair = QPushButton("Sair")
+        layoutMenuLateral.addWidget(botaoSair)
         
         menuLateral.setLayout(layoutMenuLateral)
         return menuLateral
